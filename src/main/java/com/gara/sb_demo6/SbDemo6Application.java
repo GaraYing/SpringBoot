37725429f1f05b5950c4d2c81b9001fe327e0d03
GaraYing <984851486@qq.com>
2018-03-08 16:49:03 +0800

    -- for a git push test

--- src/main/java/com/gara/sb_demo6/SbDemo6Application.java
+++ src/main/java/com/gara/sb_demo6/SbDemo6Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbDemo6Application {
-
+	// for a git push test
 	public static void main(String[] args) {
+
 		SpringApplication.run(SbDemo6Application.class, args);
 	}
 }
