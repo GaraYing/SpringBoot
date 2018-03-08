634e606c9a4cc02c45d2b49be316d95de663e65e
GaraYing <984851486@qq.com>
2018-03-08 15:13:10 +0800

    first commit

---
+++ src/main/java/com/gara/sb_demo6/SbDemo6Application.java
@@ -0,0 +1,12 @@
+package com.gara.sb_demo6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDemo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDemo6Application.class, args);
+	}
+}
---
+++ src/test/java/com/gara/sb_demo6/SbDemo6ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gara.sb_demo6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbDemo6ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
