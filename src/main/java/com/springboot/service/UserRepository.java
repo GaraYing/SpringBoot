e4e188a4dff38e1bc291fe0c11f4757d9687bf87
GaraYing <984851486@qq.com>
2018-03-19 14:06:52 +0800

    delete files

--- src/main/java/com/springboot/service/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.service;
-
-import com.springboot.vo.User3;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User3,Long> {
-
-    User3 findByUserName(String userName);
-}
