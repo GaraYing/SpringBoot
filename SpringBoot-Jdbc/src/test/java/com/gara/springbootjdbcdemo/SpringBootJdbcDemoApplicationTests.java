7d31351e8c24610accd44f9190906d6b4e6c273b
GaraYing <984851486@qq.com>
2019-09-05 20:21:20 +0800

    -- learn shiro on springboot
    Signed-off-by: zhuo.ying <yingz@fadada.com>

--- SpringBoot-Jdbc/src/test/java/com/gara/springbootjdbcdemo/SpringBootJdbcDemoApplicationTests.java
+++ SpringBoot-Jdbc/src/test/java/com/gara/springbootjdbcdemo/SpringBootJdbcDemoApplicationTests.java
@@ -1,7 +1,9 @@
 package com.gara.springbootjdbcdemo;
 
+import com.gara.springbootjdbcdemo.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,6 +14,18 @@ public class SpringBootJdbcDemoApplicationTests {
     @Test
     public void contextLoads() {
 
+    }
+
+    @Autowired
+    private  UserRepository userRepository;
+
+//    public UserRepositoryTest(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+    @Test
+    public void findAll() {
+        System.out.println(userRepository.findAll());
     }
 }
 
