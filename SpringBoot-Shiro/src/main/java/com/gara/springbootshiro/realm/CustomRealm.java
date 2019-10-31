3200508447c54e3b2dab7e7e4a927788420e6f52
GaraYing <984851486@qq.com>
2019-10-31 16:11:30 +0800

    -- polish code
    Signed-off-by: GaraYing <984851486@qq.com>

--- SpringBoot-Shiro/src/main/java/com/gara/springbootshiro/realm/CustomRealm.java
+++ SpringBoot-Shiro/src/main/java/com/gara/springbootshiro/realm/CustomRealm.java
@@ -13,7 +13,6 @@ import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Set;
