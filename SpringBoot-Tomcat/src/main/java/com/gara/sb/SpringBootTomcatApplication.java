70ec96a260333b986609e1fb2fd1ff2fc9a7bb9f
GaraYing <984851486@qq.com>
2019-12-11 17:16:48 +0800

    learn: SpringApplicationBuilder
    Signed-off-by: GaraYing <984851486@qq.com>

--- SpringBoot-Tomcat/src/main/java/com/gara/sb/SpringBootTomcatApplication.java
+++ SpringBoot-Tomcat/src/main/java/com/gara/sb/SpringBootTomcatApplication.java
@@ -1,16 +1,45 @@
 package com.gara.sb;
 
-import org.springframework.boot.SpringApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
 /**
  * @author GARA
  */
+@Slf4j
 @SpringBootApplication
 public class SpringBootTomcatApplication {
 
+	@Bean
+	public ServletContextListener contextListener(){
+		return new ServletContextListener() {
+			@Override
+			public void contextInitialized(ServletContextEvent sce) {
+				log.info("servletContext initialized...");
+			}
+
+			@Override
+			public void contextDestroyed(ServletContextEvent sce) {
+				log.info("servletContext destroyed...");
+			}
+		};
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTomcatApplication.class, args);
+
+		new SpringApplicationBuilder(SpringBootTomcatApplication.class)
+				.bannerMode(Banner.Mode.CONSOLE)
+				.web(WebApplicationType.SERVLET)
+				.build()
+				.run(args);
+//		SpringApplication.run(SpringBootTomcatApplication.class, args);
 	}
 
 }
