2d14ab1c964064f1330475c30b33efaa493012d9
GaraYing <984851486@qq.com>
2019-12-12 16:23:05 +0800

    combination of one feauture
    learn: 1. group validation; 2. GlobalExceptionHandle
    
    Signed-off-by: GaraYing <984851486@qq.com>
    
    learn: 1. group validation; 2. GlobalExceptionHandle(add files)
    
    Signed-off-by: GaraYing <984851486@qq.com>
    
    learn: 1. group validation; 2. GlobalExceptionHandle(add  file)
    
    Signed-off-by: GaraYing <984851486@qq.com>

--- SpringBoot-Validation/src/main/java/com/gara/sb/controller/CustomerController.java
+++ SpringBoot-Validation/src/main/java/com/gara/sb/controller/CustomerController.java
@@ -1,18 +1,25 @@
 package com.gara.sb.controller;
 
+import com.gara.sb.domain.Customer;
 import com.gara.sb.service.CustomerService;
+import com.gara.sb.validation.CustomerGroup;
+import com.gara.sbcommon.result.Result;
+import com.gara.sbcommon.result.ResultGenerator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.concurrent.CompletableFuture;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @description:
  * @createTime: 2019-10-08 10:13
  * @Version: 4.0
  **/
-@Controller
+@RestController
+@Slf4j
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -22,37 +29,15 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @PostMapping(value = "/test/group/validate")
+    public Result<Customer> testGroupValidate(@RequestBody @Validated({CustomerGroup.class}) Customer customer){
+      log.info("request params is: {}", customer.toString() );
+        return ResultGenerator.genSuccessResult(customerService.addCustomer(customer));
+    }
+
     @GetMapping("testSuperBuilder")
     public String testSuperBuilder() {
         customerService.testSuperBuilder();
         return "success";
-    }
-
-    public static void main(String[] args) {
-        long currentTimeMillis = System.currentTimeMillis();
-        String s = CompletableFuture.supplyAsync(CustomerController::hello)
-                .thenCombine(
-                        CompletableFuture.supplyAsync(
-                                CustomerController::world), (s1, s2) -> s1 + " " + s2).join();
-        System.out.println(s);
-        System.out.println(System.currentTimeMillis() - currentTimeMillis + "ms");
-    }
-
-    private static String hello(){
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "hello";
-    }
-
-    private static String world(){
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "world";
     }
 }
---
+++ SpringBoot-Validation/src/main/java/com/gara/sb/validation/UserGroup.java
@@ -0,0 +1,10 @@
+package com.gara.sb.validation;
+
+/**
+ * @description: User验证组
+ * @author:  Gara
+ * @createTime: 2019/12/12 10:46
+ * @Version: 1.0
+**/
+public interface UserGroup {
+}
