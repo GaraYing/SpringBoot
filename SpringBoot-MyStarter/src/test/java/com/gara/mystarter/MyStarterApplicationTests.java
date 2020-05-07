bd3773235b386e5678e36d02ae50e8d67de160a4
GaraYing <984851486@qq.com>
2020-05-07 19:57:46 +0800

    code polish
    Signed-off-by: GaraYing <984851486@qq.com>

--- SpringBoot-MyStarter/src/test/java/com/gara/mystarter/MyStarterApplicationTests.java
+++ SpringBoot-MyStarter/src/test/java/com/gara/mystarter/MyStarterApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MystarterApplicationTests {
+public class MyStarterApplicationTests {
 
     @Test
     public void contextLoads() {
