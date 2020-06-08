1c07cbacf6e92ce43fe0862e4b2095d91bc8c6c8
GaraYing <984851486@qq.com>
2020-06-08 19:59:08 +0800

    feat: duduce WebApplicationType and MainClass in SpringBoot Application
    Signed-off-by: GaraYing <984851486@qq.com>

--- SpringBoot-Validation/src/main/java/com/gara/ValidationDemoApplication.java
+++ SpringBoot-Validation/src/main/java/com/gara/ValidationDemoApplication.java
@@ -5,13 +5,18 @@ import com.gara.sb.property.SampleProperties;
 import com.gara.sb.validation.SamplePropertiesValidator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.Validator;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+import java.util.HashSet;
+import java.util.Set;
+
+//@SpringBootApplication
 public class ValidationDemoApplication implements WebMvcConfigurer, CommandLineRunner {
 
 	private final SampleProperties properties;
@@ -41,6 +46,22 @@ public class ValidationDemoApplication implements WebMvcConfigurer, CommandLineR
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(ValidationDemoApplication.class, args);
+//		SpringApplication.run(ValidationDemoApplication.class, args);
+
+		Set<String> sources = new HashSet<>();
+		sources.add(ApplicationConfiguration.class.getName());
+
+		SpringApplication.run(ApplicationConfiguration.class, args);
+		SpringApplication springApplication = new SpringApplication();
+		springApplication.setSources(sources);
+		springApplication.setWebApplicationType(WebApplicationType.NONE);
+
+		ConfigurableApplicationContext context = springApplication.run();
+		System.out.println("ValidationDemoApplication.main: " + context.getBean(ApplicationConfiguration.class));
+	}
+
+	@SpringBootApplication
+	public static class ApplicationConfiguration{
+
 	}
 }
