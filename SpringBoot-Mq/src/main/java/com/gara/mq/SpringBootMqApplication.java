b7a4f3a0b166085218498a347d0dbcfa3f6b9a79
GaraYing <984851486@qq.com>
2020-12-27 21:30:00 +0800

    feat: new project:SpringBootMq

--- SpringBoot-Mq/src/main/java/com/gara/mq/SpringBootMqApplication.java
+++ SpringBoot-Mq/src/main/java/com/gara/mq/SpringBootMqApplication.java
@@ -1,13 +1,12 @@
-package com.gara.test;
+package com.gara.mq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestApplication {
+public class SpringBootMqApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestApplication.class, args);
+        SpringApplication.run(SpringBootMqApplication.class, args);
     }
-
 }
---
+++ SpringBoot-Mq/src/test/java/com/gara/mq/SpringBootMqApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gara.mq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMqApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
